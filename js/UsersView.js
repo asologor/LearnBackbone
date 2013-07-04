f57fa66b32c5ba12656abbae49c905c581cd84f1
Andrey Sologor <asologor@gmail.com>
2013-07-04 19:21:47 +0300

    newVer3

--- js/UsersView.js
+++ js/UsersView.js
@@ -8,7 +8,16 @@
 var UsersView = Backbone.View.extend({
     tagName: 'ol',
 
+    initialize: function(){
+        this.collection.on('click', this.clicked, this);
+    },
+
+    clicked: function(){
+        this.$('.selected').removeClass('selected');
+    },
+
     render: function(){
+        console.log(this);
         this.collection.each(function(user){
             var userView = new View({model: user});
             this.$el.append(userView.render().el);
